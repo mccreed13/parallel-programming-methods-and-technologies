d58745120a44299e5a5c9c9df12dfb467e04dc36
mccreed13 <a.lapushenko13@gmail.com>
2026-03-01 15:38:33 +0200

    PiCalculator with list number of threads was added.

--- lab1/src/main/java/org/example/CPUBound/PiCalculator.java
+++ lab1/src/main/java/org/example/CPUBound/PiCalculator.java
@@ -10,20 +10,15 @@ public class PiCalculator {
     private final static SecureRandom random = new SecureRandom();
 
     final static int TOTAL_POINTS = 1_000_000_000;
-    final static int NUMBER_OF_THREADS = 2;
+    private static final List<Integer> THREADS = List.of(1,6,12);
 
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
-        System.out.println(PiCalculator.calculate(TOTAL_POINTS, NUMBER_OF_THREADS));
-        System.out.println("Час паралельного виконання: " + (System.currentTimeMillis() - startTime) + " мс");
-
-        startTime = System.currentTimeMillis();
-        System.out.println(PiCalculator.calculate(TOTAL_POINTS));
-        System.out.println("Час послідовного виконання: " + (System.currentTimeMillis() - startTime) + " мс");
-    }
-
-    public static double calculate(int totalPoints) {
-        return calculate(totalPoints, 1);
+        for(int threads: THREADS) {
+            long startTime = System.currentTimeMillis();
+            System.out.println(PiCalculator.calculate(TOTAL_POINTS, threads));
+            System.out.println("Threads: " + threads);
+            System.out.println("Час виконання: " + (System.currentTimeMillis() - startTime) + " мс");
+        }
     }
 
     public static double calculate(int totalPoints, int numberOfThreads) {
