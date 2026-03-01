86c9c98665a09f587812bd26fad7e99669414685
mccreed13 <a.lapushenko13@gmail.com>
2026-03-01 19:44:03 +0200

    ParallelMatrixTranspose created

---
+++ lab1/src/main/java/org/example/MemoryBound/ParallelMatrixTranspose.java
@@ -0,0 +1,51 @@
+package org.example.MemoryBound;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ParallelMatrixTranspose {
+    private static final int SIZE = 10000;
+    private static final double[] MATRIX = new double[SIZE * SIZE];
+    private static final List<Integer> THREADS = List.of(1,6,12);
+
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < MATRIX.length; i++) {
+            MATRIX[i] = i;
+        }
+        for (int threads : THREADS) {
+            System.out.println("Кількість потоків: " + threads);
+            long startTime = System.currentTimeMillis();
+            transpose(threads);
+            System.out.println("Час виконання: " + (System.currentTimeMillis() - startTime) + " мс");
+        }
+    }
+
+    private static void transpose(int numThreads) throws InterruptedException {
+        double[] result = new double[SIZE * SIZE];
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+
+        int rowsPerThread = SIZE / numThreads;
+
+        for (int t = 0; t < numThreads; t++) {
+            final int startRow = t * rowsPerThread;
+            final int endRow = (t == numThreads - 1) ? SIZE : (startRow + rowsPerThread);
+
+            executor.submit(() -> {
+                for (int i = startRow; i < endRow; i++) {
+                    for (int j = 0; j < SIZE; j++) {
+                        result[j * SIZE + i] = MATRIX[i * SIZE + j];
+                    }
+                }
+            });
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.MINUTES);
+
+        //Перевірка
+         assert result[SIZE] == MATRIX[1];
+    }
+}
